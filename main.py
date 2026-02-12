c5383b51c53c53becc4a99ca8b702bca0a296d5c
Enkhjin <2026064@miu.edu.mn>
2026-02-12 11:11:36 +0800

    First Commit

---
+++ main.py
@@ -0,0 +1,41 @@
+# print("hello world")
+# name="enkhjin"
+# age=20
+# hobby="tour"
+# print(f"i am +{name}+{age}yaers old+i like+{hobby}")
+# age=input("how old are you:")
+# a=int(input("a:"))
+# b=int(input("b:"))
+# print(a+b)
+
+
+# age=2
+# if age>21:
+#     print("yes.")
+
+# elif age>1:
+#     print("no.")
+     
+# else:
+#     print("error.")
+    
+#A,B,C,D,F
+#90>=A
+#80>=B
+#70>=C
+#60>=D
+#59>=F
+grade = 58
+if grade >=90:
+    print("A")
+elif grade >=80:
+    print("B")
+elif grade>=70:
+    print("c")
+elif grade>=60:
+    print("D")
+else:
+    print("F")
+
+     
+
---
+++ main2.py
@@ -0,0 +1,48 @@
+# #for loop
+# for x in reversed (range(0,11)):
+#     print(x)
+
+
+
+
+# # fruits = ["apple","orange","grape","banana"]
+# # print(fruits[0])
+# # print(fruits[-1])
+
+
+# fruits = ["apple","orange","grape","banana","mango"]
+# for fruit in fruits:
+#     print("bought:",fruit)
+
+
+
+
+
+
+
+# scores = [23, 45, 67, 89, 90, 99]
+# for score in scores:
+#     if score > 60:
+#         print("pass:",score)
+#     else:
+#         print("fall:",score)
+
+
+
+
+
+
+
+
+nums = [2,3,4,5]
+         
+for num in nums:
+     print(num**3)
+ 
+ 
+
+
+
+
+
+
